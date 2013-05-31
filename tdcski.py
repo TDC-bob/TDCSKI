5bbc8fc6519924c6e47b4dae9f6425d7684ce6b0
bob <dariboufix@daribouca.net>
2013-05-31 12:27:03 +0200

    moved ENV to beginning of script, silly me

--- tdcski.py
+++ tdcski.py
@@ -21,6 +21,8 @@ from _logging._logging import mkLogger, DEBUG
 logger = mkLogger(__name__, DEBUG)
 
 def main():
+    os.environ["GIT_SSL_NO_VERIFY"] = "1"
+
     main_repo = git.Repo(".","https://github.com/TDC-bob/TDCSKI.git")
     print(main_repo.remotes)
     main_repo.pull()
@@ -32,7 +34,6 @@ def main():
 
     if not os.path.exists("../mods"):
         os.makedirs("../mods")
-    os.environ["GIT_SSL_NO_VERIFY"] = "1"
     git.Repo("../mods/modlist", "https://github.com/TDC-bob/modlist.git").pull()
     sys.path.append(os.path.abspath("../modlist/"))
     # noinspection PyUnresolvedReferences
