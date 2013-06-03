823e06ce94a2d0416f1b18dfa2c3711509eb9eba
bob <dariboufix@daribouca.net>
2013-06-03 13:20:36 +0200

    t

--- tdcski.py
+++ tdcski.py
@@ -70,14 +70,11 @@ def main():
     config.git_exe = conf.get("general", "git_path")
     config.SaveGames_path = conf.get("general", 'saved_games_path')
     config.DCS_path = conf.get("general", 'dcs_path')
-    logger.error(config.SaveGames_path)
-    exit(0)
 
     if not os.path.exists("../repos/skins"):
         os.makedirs("../repos/skins")
     if not os.path.exists("../repos/mods"):
         os.makedirs("../repos/mods")
-
 
     Repo("../repos/list", "https://github.com/TDC-bob/modlist.git").pull()
 
