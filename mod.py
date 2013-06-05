d835c1e375f12c39fea635b6cfc8cbbcbdc9b7ed
bob <dariboufix@daribouca.net>
2013-06-05 14:00:13 +0200

    debugging

--- mod.py
+++ mod.py
@@ -221,6 +221,7 @@ class Mod():
                     with open(mod_file.full_path, mode='r') as file:
                         lines = file.readlines()
                         for line in lines:
+                            orig_line = line
                             self.logger.debug("\tligne: {}".format(line))
                             re_1 = re.compile(r"\$\$SAVED_GAMES\$\$")
                             re_2 = re.compile(r"\$\$DCS\$\$")
@@ -229,9 +230,15 @@ class Mod():
                             self.logger.debug("line: {}".format(line))
                             self.logger.debug("UTF8: {}".format(line.encode()))
                             lines_to_add.append(line)
+                            line = orig_line
                             line = re.sub(re_1, re.escape(config.SaveGames_path), line)
                             line = re.sub(re_2, re.escape(config.DCS_path), line)
                             self.logger.debug("escaped line: {}".format(line))
+                            lines_to_add.append(line)
+                            line = orig_line
+                            line = line.replace("$$SAVED_GAMES$$", config.SaveGames_path)
+                            line = line.replace("$$DCS$$", config.DCS_path)
+                            self.logger.debug("str.replace: {}".format(line))
                             lines_to_add.append(line)
                     self.logger.debug("lecture du fichier à éditer")
                     with open(mod_file.install_to, mode='r', encoding="UTF-8") as file:
